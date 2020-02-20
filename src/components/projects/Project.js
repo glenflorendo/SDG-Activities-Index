ef0931a875b6a16c059cfc62b82d9f44bff0c9f0
lindamejia <lindaisamarmejia@gmail.com>
2020-02-20 11:02:55 -0800

    reformatted card layout

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardColumns, Container } from "react-bootstrap";
+import { Card, Row, Col, Container } from "react-bootstrap";
 import style from "./Project.module.css";
 import Fade from "react-reveal/Fade";
 
@@ -38,7 +38,7 @@ class Project extends React.Component {
       })
     );
     const sdgImages = finalArray.map((data, index) => (
-      <img key={index} src={data} width="auto" height="50" alt="goal" />
+      <img key={index} src={data} width="auto" height="60" alt="goal" />
     ));
     return sdgImages;
   };
@@ -58,61 +58,66 @@ class Project extends React.Component {
     return (
       <div className={style.projects}>
         <Container>
-          <CardColumns>
+          <Row>
             {this.props.projects.map((data, index) => (
-              <div
-                key={data.id}
-                onClick={() => this.flipCard(data.id)}
-                className={style.card}
-              >
-                {!this.state.flipped[data.id] ? (
-                  <Fade bottom>
+              <Col sm="12" lg="4">
+                <div
+                  key={data.id}
+                  onClick={() => this.flipCard(data.id)}
+                  className={style.card}
+                >
+                  {!this.state.flipped[data.id] ? (
+                    <Fade bottom>
+                      <Card
+                        style={{ marginTop: "20px", textAlign: "center" }}
+                        className={`${style.front} mb-4 p-3`}
+                      >
+                        <p style={{ color: "#a6a6a6" }}>{data.sector}</p>
+                        <Card.Body>
+                          <Card.Title style={{ fontSize: "20px" }}>
+                            {data.projectname}
+                          </Card.Title>
+                          <Card.Text>{data.organization}</Card.Text>
+                        </Card.Body>
+                        <Card.Text>{this.getSdgImages(data.sdg)}</Card.Text>
+                        <div
+                          className={style.readMore}
+                          style={{ display: "inline-block" }}
+                        >
+                          {this.displayThemes(data.theme)}
+                        </div>
+                      </Card>
+                    </Fade>
+                  ) : (
                     <Card
-                      style={{ marginTop: "20px", textAlign: "center" }}
-                      className={`${style.front} mb-4 p-3`}
+                      style={{
+                        color: "white",
+                        marginTop: "20px",
+                        textAlign: "center",
+                        backgroundColor:
+                          data.activitytype === "organization"
+                            ? "#ff9244"
+                            : "#2c88c8"
+                      }}
+                      className={`${style.back}  p-3`}
                     >
-                      <p style={{ color: "#a6a6a6" }}>{data.sector}</p>
-                      <Card.Body>
-                        <Card.Title style={{ fontSize: "20px" }}>
-                          {data.projectname}
-                        </Card.Title>
-                        <Card.Text>{data.organization}</Card.Text>
-                      </Card.Body>
-                      <Card.Text>{this.getSdgImages(data.sdg)}</Card.Text>
-                      <div style={{ display: "inline-block" }}>
-                        {this.displayThemes(data.theme)}
-                      </div>
+                      <i className="fas fa-undo" style={{ float: "right" }}></i>
+                      <Card.Title>{data.projectname}</Card.Title>
+                      <Card.Text>{data.organization}</Card.Text>
+                      <small>{data.description}</small>
+                      <br />
+                      <Card.Link
+                        className={style.readMore}
+                        href={`${data.website}`}
+                      >
+                        Read More
+                      </Card.Link>
                     </Card>
-                  </Fade>
-                ) : (
-                  <Card
-                    style={{
-                      color: "white",
-                      marginTop: "20px",
-                      textAlign: "center",
-                      backgroundColor:
-                        data.activitytype === "organization"
-                          ? "#ff9244"
-                          : "#2c88c8"
-                    }}
-                    className={`${style.back} p-3`}
-                  >
-                    <i className="fas fa-undo" style={{ float: "right" }}></i>
-                    <Card.Title>{data.projectname}</Card.Title>
-                    <Card.Text>{data.organization}</Card.Text>
-                    <small>{data.description}</small>
-                    <br />
-                    <Card.Link
-                      className={style.readMore}
-                      href={`${data.website}`}
-                    >
-                      Read More
-                    </Card.Link>
-                  </Card>
-                )}
-              </div>
+                  )}
+                </div>
+              </Col>
             ))}
-          </CardColumns>
+          </Row>
         </Container>
       </div>
     );
