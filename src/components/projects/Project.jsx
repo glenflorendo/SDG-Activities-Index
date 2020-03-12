4c49a0df6b7ccf50c998099f722df46649ce2029
lindamejia <lindaisamarmejia@gmail.com>
2020-03-12 10:44:05 -0700

    Added Card Columns for mosaic layout, on flipped is stable

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,29 +1,28 @@
 import React from "react";
-import { Card, Row, Col, Container } from "react-bootstrap";
+import { Card, Container, CardColumns } from "react-bootstrap";
 import style from "./Project.module.css";
 import Fade from "react-reveal/Fade";
 
 class Project extends React.Component {
   state = {
     flipped: {},
-    backgroundColor: ""
+    backgroundColor: "",
+    height: {}
   };
 
   flipCard = id => {
+    let cardHeight = document.getElementById(id).offsetHeight;
+    console.log(cardHeight);
     this.setState(prevState => ({
       flipped: {
         ...prevState.flipped,
         [id]: !prevState.flipped[id]
+      },
+      height: {
+        ...prevState.height,
+        [id]: cardHeight
       }
     }));
-  };
-
-  backgroundColor = () => {
-    if (this.props.projects.actvityType === "organization") {
-      this.setState({ backgroundColor: "primary" });
-    } else if (this.props.projects.actvityType === "project") {
-      this.setState({ backgroundColor: "warning" });
-    }
   };
 
   getSdgImages = projectSDGs => {
@@ -58,72 +57,78 @@ class Project extends React.Component {
     return (
       <div className={style.projects}>
         <Container>
-          <Row>
+          <CardColumns>
             {this.props.projects.map((data, index) => (
-              <Col key={data.id} sm="12" lg="4">
-                <Fade bottom>
-                  <div
-                    onClick={() => this.flipCard(data.id)}
-                    className={style.card}
-                  >
-                    {!this.state.flipped[data.id] ? (
-                      <Card
-                        style={{ marginTop: "20px", textAlign: "center" }}
-                        className="mb-4 p-3"
-                      >
-                        <p style={{ color: "#a6a6a6" }}>{data.sector}</p>
-                        <Card.Body>
-                          <Card.Title style={{ fontSize: "20px", color:
-                            data.activitytype === "organization"
-                              ? "#ff9244"
-                              : "#2c88c8" }}>
-                            {data.projectname}
-                          </Card.Title>
-                          <Card.Text>{data.organization}</Card.Text>
-                        </Card.Body>
-                        <Card.Text>{this.getSdgImages(data.sdg)}</Card.Text>
-                        <div
-                          className={style.themes}
-                          style={{ display: "inline-block" }}
+              <Fade key={data.id} bottom>
+                <div
+                  onClick={() => this.flipCard(data.id)}
+                  className={style.card}
+                >
+                  {!this.state.flipped[data.id] ? (
+                    <Card
+                      style={{ marginTop: "20px", textAlign: "center" }}
+                      className="mb-4 p-3"
+                      id={data.id}
+                    >
+                      <p style={{ color: "#a6a6a6" }}>{data.sector}</p>
+                      <Card.Body>
+                        <Card.Title
+                          style={{
+                            fontSize: "20px",
+                            color:
+                              data.activitytype === "organization"
+                                ? "#ff9244"
+                                : "#2c88c8"
+                          }}
                         >
-                          {this.displayThemes(data.theme)}
-                        </div>
-                      </Card>
-                    ) : (
-                      <Card
-                        style={{
-                          color: "white",
-                          marginTop: "20px",
-                          textAlign: "center",
-                          backgroundColor:
-                            data.activitytype === "organization"
-                              ? "#ff9244"
-                              : "#2c88c8"
-                        }}
-                        className="p-3"
-                      >
-                        <i
-                          className="fas fa-undo"
-                          style={{ marginBottom: "10px" }}
-                        ></i>
-                        <Card.Title>{data.projectname}</Card.Title>
+                          {data.projectname}
+                        </Card.Title>
                         <Card.Text>{data.organization}</Card.Text>
-                        <small>{data.description}</small>
-                        <br />
-                        <a
-                          className={style.readMore}
-                          href={`${data.website}`}
-                          target="_blank"
-                        >
-                          Read More
-                        </a>
-                      </Card>
-                    )}
-                  </div>
-                </Fade>
-              </Col>
+                      </Card.Body>
+                      <Card.Text>{this.getSdgImages(data.sdg)}</Card.Text>
+                      <div
+                        className={style.themes}
+                        style={{ display: "inline-block" }}
+                      >
+                        {this.displayThemes(data.theme)}
+                      </div>
+                    </Card>
+                  ) : (
+                    <Card
+                      style={{
+                        height: this.state.height[data.id],
+                        overflow: "auto",
+                        color: "white",
+                        marginTop: "20px",
+                        textAlign: "center",
+                        backgroundColor:
+                          data.activitytype === "organization"
+                            ? "#ff9244"
+                            : "#2c88c8"
+                      }}
+                      className="p-3"
+                    >
+                      <i
+                        className="fas fa-undo"
+                        style={{ marginBottom: "10px" }}
+                      ></i>
+                      <Card.Title>{data.projectname}</Card.Title>
+                      <Card.Text>{data.organization}</Card.Text>
+                      <small>{data.description}</small>
+                      <br />
+                      <a
+                        className={style.readMore}
+                        href={`${data.website}`}
+                        target="_blank"
+                      >
+                        Read More
+                      </a>
+                    </Card>
+                  )}
+                </div>
+              </Fade>
             ))}
-          </Row>
+          </CardColumns>
         </Container>
       </div>
     );
